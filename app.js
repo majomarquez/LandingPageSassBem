15d1fdff7911ba919fa75d9aa59cc316c943eab5
MJM <47110521+majomarquez@users.noreply.github.com>
2022-05-18 13:06:49 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// Carousel
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop: true
+});
+// Initialize Swiper
+
+
